84b45136011b3ad22f1002586890bb73dd7f95c1
undefined <sjx233@qq.com>
2020-08-15 19:14:31 +0800

    Update fabric
    Signed-off-by: undefined <sjx233@qq.com>

--- src/main/java/io/github/sjx233/ironrust/world/entity/IronRustEntityType.java
+++ src/main/java/io/github/sjx233/ironrust/world/entity/IronRustEntityType.java
@@ -1,34 +1,37 @@
 package io.github.sjx233.ironrust.world.entity;
 
 import io.github.sjx233.ironrust.IronRustMod;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.SpawnRestriction;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.Heightmap;
 
 public enum IronRustEntityType {
   ;
-  public static final EntityType<RustyCow> RUSTY_COW = register("rusty_cow", FabricEntityTypeBuilder
-    .create(SpawnGroup.MONSTER, RustyCow::new)
-    .dimensions(EntityDimensions.changing(0.9f, 1.4f))
-    .build());
-  public static final EntityType<RustPowder> RUST_POWDER = register("rust_powder", FabricEntityTypeBuilder
-    .<RustPowder>create(SpawnGroup.MISC, RustPowder::new)
-    .dimensions(EntityDimensions.changing(0.25f, 0.25f))
-    .trackable(4, 10)
-    .build());
-
-  static {
-    FabricDefaultAttributeRegistry.register(RUSTY_COW, HostileEntity.createHostileAttributes()
-      .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.2d)
-      .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 5d));
-  }
+  public static final EntityType<RustyCow> RUSTY_COW = register("rusty_cow",
+    FabricEntityTypeBuilder.<RustyCow>createMob()
+      .spawnGroup(SpawnGroup.MONSTER)
+      .entityFactory(RustyCow::new)
+      .dimensions(EntityDimensions.changing(0.9f, 1.4f))
+      .defaultAttributes(() ->
+        HostileEntity.createHostileAttributes()
+          .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.2d)
+          .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 5d))
+      .spawnRestriction(SpawnRestriction.Location.NO_RESTRICTIONS, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, HostileEntity::canSpawnInDark)
+      .build());
+  public static final EntityType<RustPowder> RUST_POWDER = register("rust_powder",
+    FabricEntityTypeBuilder.<RustPowder>create()
+      .entityFactory(RustPowder::new)
+      .dimensions(EntityDimensions.changing(0.25f, 0.25f))
+      .trackable(4, 10)
+      .build());
 
   private static <T extends Entity> EntityType<T> register(String id, EntityType<T> type) {
     return register(IronRustMod.makeId(id), type);
